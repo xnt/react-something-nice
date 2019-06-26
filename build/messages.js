2d55201dcfcfd15fb7a07f52ff83e316979ee139
Vicente Plata <vplata@forhims.com>
2019-06-25 22:10:28 -0400

    🏗  Update build

--- build/messages.js
+++ build/messages.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var messages = ["Thank you for your hard work ✨", "Have a great day ☕️!", "Your engineering team wishes you a fantastic day ♥"];
+var messages = ["Have a great day ☕️!", "Lets' continue bringing out the best in one another 👌", "Thank you for your dedication, your time and your talent 👍", "Thank you for your hard work ✨", "Thank you 👏!", "We're glad that you're part of our team 🤗", "Your engineering team wishes you a fantastic day ♥"];
 
 var getMessage = function getMessage() {
   return messages[Math.floor(Math.random() * messages.length)];
