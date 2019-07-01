7a2859c13598a48f251b4c8f4f0c5859e1b4d938
Nourhan Anber <nourhan.anber@gmail.com>
2019-06-30 22:36:41 -0400

    21 new message added

--- src/messages.js
+++ src/messages.js
@@ -7,7 +7,29 @@ const messages = [
   "Thank you for your hard work ✨",
   "Thank you 👏!",
   "We're glad that you're part of our team 🤗",
-  "Your engineering team wishes you a fantastic day ♥"
+  "Your engineering team wishes you a fantastic day ♥",
+  "Keep calm & good luck in the new job 🤗",
+  "Your hard work and effort have paid off! A success well deserved, an occasion worth celebrating! Congratulations!👍",
+  "If Oscars were given for a job well done, I’d nominate you! Congratulations for your fantastic achievement!👌",
+  "Alone we can do so much little, together we can do so much. Your dedication and service is appreciated.",
+  "Just an opportunity to say ‘wow’ and ‘ThankYou’ for all your great work.👊",
+  "This is truly above and beyond. Keep it up.",
+  "Congratulations for your fabulous victory! You deserve it every bit! Aim for the stars!🎉",
+  "May success never leave your hand, and may you get everything you want. Congratulations.🎉",
+  "Pursuing excellence is not a one time job. It is a way of life. Good job.",
+  "The first step in a long journey of perfection and success starts with a good job.",
+  "The reward for work well done is the opportunity to do more.",
+  "I appreciate your quick response to help out a new customer!",
+  "Keep calm and keep up the good work.",
+  "You’ve got a winner’s attitude. Of course! You are a winner!👊",
+  "Many thanks for all your hard work, we are so thankful for all that you do!",
+  "Your dedication contributes to our success. You are a ROCKSTAR at work..!!!",
+  "Good things come to those who work hard. Keep up the good work.",
+  "I really like what you are doing, Keep up the good work.",
+  "You did an excellent job.Thanks for being Awesome.",
+  "A great employee is like a four leaf clover, hard to find & lucky to have.👊",
+  "We can’t spell SUCCESS without U..!!🎉",
+  "Love having you on the team. Thanks for all that you do."
 ];
 
 const getMessage = () => {
