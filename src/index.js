ad70e722a447834f51c7fb1621a475ae8144116e
Hugo Noe LESTA <u0162683@naranja.com>
2019-07-09 12:16:45 -0300

    simplified language check and deleted console.logs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 import getMessage from './messages';
-import AllowedLanguages from './allowedLanguages';
 import React, { PureComponent } from 'react';
 
 export default class SomethingNice extends PureComponent {
