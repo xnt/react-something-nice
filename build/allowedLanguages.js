481f7fb5bf75e543b4cf0f3ae2e9ca6941912b27
Hugo Noe LESTA <u0162683@naranja.com>
2019-07-09 01:28:27 -0300

    added new language ES and EN as default.

---
+++ build/allowedLanguages.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var AllowedLanguages = ['es', 'en'];
+var _default = AllowedLanguages;
+exports["default"] = _default;
--- build/messages.js
+++ build/messages.js
@@ -5,9 +5,21 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 var messages = ["Have a great day ☕️!", "Lets' continue bringing out the best in one another 👌", "Thank you for your dedication, your time and your talent 👍", "Thank you for your hard work ✨", "Thank you 👏!", "We're glad that you're part of our team 🤗", "Your engineering team wishes you a fantastic day ♥"];
+var mensajes = ["Que tengas un buen día = !", "Gracias por trabajar tanto!", "Muchas gracias :)"];
 
-var getMessage = function getMessage() {
-  return messages[Math.floor(Math.random() * messages.length)];
+var languages = function languages(lang) {
+  var l = {
+    en: messages,
+    es: mensajes
+  };
+  return l[lang];
+};
+
+var getMessage = function getMessage(lang) {
+  console.log("Lang en getMessage ".concat(lang));
+  if (!lang) lang = 'en';
+  var idiom = languages(lang);
+  return idiom[Math.floor(Math.random() * idiom.length)];
 };
 
 var _default = getMessage;
