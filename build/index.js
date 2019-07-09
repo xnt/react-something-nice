db223082a29bd2090fb81adea5d90aedd9745325
Hugo Noe LESTA <u0162683@naranja.com>
2019-07-09 12:16:45 -0300

    simplified language check and deleted console.logs

--- build/index.js
+++ build/index.js
@@ -7,8 +7,6 @@ exports["default"] = void 0;
 
 var _messages = _interopRequireDefault(require("./messages"));
 
-var _allowedLanguages = _interopRequireDefault(require("./allowedLanguages"));
-
 var _react = _interopRequireWildcard(require("react"));
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
