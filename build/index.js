ef4cef83ad3142804ee45b4d0d8549c4894cb6e4
Vicente Plata <vplata@forhims.com>
2019-07-12 21:46:36 -0400

    Prepare v0.4.0
    Also, ignore build directory from linting

--- build/index.js
+++ build/index.js
@@ -9,11 +9,9 @@ var _messages = _interopRequireDefault(require("./messages"));
 
 var _allowedLanguages = _interopRequireDefault(require("./allowedLanguages"));
 
-var _react = _interopRequireWildcard(require("react"));
+var _react = require("react");
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -53,7 +51,7 @@ function (_PureComponent) {
   _createClass(SomethingNice, [{
     key: "render",
     value: function render() {
-      return _react["default"].createElement("span", {
+      return React.createElement("span", {
         style: this.props.style,
         className: this.props.className
       }, this.message);
