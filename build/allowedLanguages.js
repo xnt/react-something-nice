366bd11ad1f049340d42418a6ead5b381315cda7
Jaza <49951284+jaza-k@users.noreply.github.com>
2019-10-01 19:25:23 -0600

    Updated allowedLanguages.js in build
    Added French language option in the array for allowed languages

--- build/allowedLanguages.js
+++ build/allowedLanguages.js
@@ -4,6 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var AllowedLanguages = ['es', 'en'];
+var AllowedLanguages = ['es', 'en', 'fr'];
 var _default = AllowedLanguages;
 exports["default"] = _default;
