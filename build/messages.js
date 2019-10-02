3a81dac85055999a1cb5749a837436eb9552bbc0
Jaza <49951284+jaza-k@users.noreply.github.com>
2019-10-01 19:24:23 -0600

    Updated messages.js in build
    added French messages array

--- build/messages.js
+++ build/messages.js
@@ -6,11 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 var messages = ["Have a great day ☕️!", "Lets' continue bringing out the best in one another 👌", "Thank you for your dedication, your time and your talent 👍", "Thank you for your hard work ✨", "Thank you 👏!", "We're glad that you're part of our team 🤗", "Your engineering team wishes you a fantastic day ♥", "You're a rockstar!", "We're lucky to have you on this team!"];
 var mensajes = ["¡Que tengas un buen día ☕️ !", "¡Gracias por trabajar tanto!", "Muchas gracias :)", "Qué bien lo estás haciendo 👌", "Gracias por tu dedicación, tu tiempo y tu talento 👍", "¡Gracias 👏!", "Estamos contentos de que seas parte de nuestro grupo 🤗", "Tu grupo de ingrenieria te desea un fantastico día ♥"];
-
+var messages_fr = [  "Bonne journée!", "Continuons à faire ressortir le meilleur l'un de l'autre!", "Merci pour votre travail acharné!", "On est heureux que vous fassiez partie de notre équipe", "Merci pour votre dévouement, votre temps et votre talent!", "Votre équipe apprécie tout ce que vous apportez à la table.", "Votre travail est apprécié ici!"];
 var languages = function languages(lang) {
   var l = {
     en: messages,
-    es: mensajes
+    es: mensajes,
+    fr: messages_fr
   };
   return l[lang];
 };
