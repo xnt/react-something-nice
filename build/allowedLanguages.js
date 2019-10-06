76462752d0c40de5d3a769a0d18325d16db58dc2
Jaza <49951284+jaza-k@users.noreply.github.com>
2019-10-05 20:30:02 -0600

    Update allowedLanguages.js

--- build/allowedLanguages.js
+++ build/allowedLanguages.js
@@ -4,6 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var AllowedLanguages = ['es', 'en', 'fr'];
+var AllowedLanguages = ['es', 'en'];
 var _default = AllowedLanguages;
 exports["default"] = _default;
